57cf506800dd9657c62de21f40a0b9627700b0ea
manicphase <thegr8dave@gmail.com>
2014-07-01 21:36:48 +0100

    updated to use sql to filter results so it runs smoother

--- app.py
+++ app.py
@@ -1,32 +1,69 @@
+import json
 import os
 import csv
-from bottle import route, run, template, static_file
+from bottle import route, run, template, static_file, request
 
 workingdir = os.getcwd()
 
 # CSV downloaded from wetroads.co.uk
+import csv
+import sqlite3
+
+
 fordtypes = {1: "icon/suitableforall.png",
             2: "icon/irishbridge.png",
             3: "icon/4by4.png",
             4: "icon/restricted.png"}
-            
+
 def assignicon(index):
     try:
         return fordtypes[int(index)]
     except:
         return "icon/unknown.png"
 
-coords = []
+conn = sqlite3.connect(":memory:")
+cur = conn.cursor()
+cur.execute("CREATE TABLE fords (LA real, LO real, CLASS text, NAME text, GRADE real);")
+
+def s(string):
+    return float(string.strip("'"))
+
 with open('Fords.csv', 'rb') as f:
-    reader = csv.reader(f)
-    reader.next()
-    for row in reader:
-        if row[0] != "":
-            coords.append([row[3], row[2], assignicon(row[8])])
+    dr = csv.DictReader(f)
+    to_db = [(s(i['LA']), s(i['LO']), assignicon(i['CLASS']), i['NAME'], s(i['GRADE'])) for i in dr if i['LA'] != ""]
+
+cur.executemany("INSERT INTO fords (LA, LO, CLASS, NAME, GRADE) VALUES (?, ?, ?, ?, ?);", to_db)
+conn.commit()
+
+cur.execute('SELECT * FROM fords ORDER BY GRADE DESC')
+coords = cur.fetchall()[:200]
+
 
 @route('/')
 def index():
+    global coords
+    try:
+        left = request.query["left"]
+        bottom = request.query["bottom"]
+        right = request.query["right"]
+        top = request.query["top"]
+        cur.execute('SELECT * FROM fords where LO>'+left+' AND LO<'+right+' AND LA>'+bottom+' AND LA<'+top+' ORDER BY GRADE DESC')
+        locations = cur.fetchall()
+    except:
+        pass
     return template(open("template.html").read(), coords = coords)
+    
+@route('/locations')
+def locations():
+    left = request.query["left"]
+    bottom = request.query["bottom"]
+    right = request.query["right"]
+    top = request.query["top"]
+    cur.execute('SELECT * FROM fords where LO>'+left+' AND LO<'+right+' AND LA>'+bottom+' AND LA<'+top+' ORDER BY GRADE DESC')
+    locations = cur.fetchall()[:200]
+    jsonobj = json.dumps(locations)
+    return jsonobj
+
     
 @route('/icon/<filename>')
 def icon(filename):
